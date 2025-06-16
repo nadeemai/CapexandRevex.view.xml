354193c931daec4f8d4bb73ac8ba953b327b3730
nadeemai <116419261+nadeemai@users.noreply.github.com>
2025-06-16 11:43:12 +0530

    Update CapexandRevex.controller.js

--- CapexandRevex.controller.js
+++ CapexandRevex.controller.js
@@ -1,3 +1,142 @@
+UPDATED CODE 2 
+
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
+], function (Controller, MessageToast, MessageBox) {
+    "use strict";
+
+    return Controller.extend("infraproject.controller.Infra", {
+        onInit: function () {
+            // Initialize the model with default data
+            var oModel = new sap.ui.model.json.JSONModel({
+                requesterTokenName: "50002151 - PRIYATHAM TELAPROLU",
+                department: "TEMP",
+                date: "2023-08-02",
+                reqNo: "REQ12345",
+                paymentOption: "Advance Payment",
+                paymentType: "Capex",
+                poNonPO: "po",
+                paymentDate: "2025-06-16", // Current date as per system
+                vendorName: "",
+                accountingDocNumber: "",
+                po: "",
+                vendorCode: "",
+                invoiceNumber: "",
+                costCenter: "",
+                wbs: "",
+                baseAmount: "",
+                gst: "",
+                tds: "",
+                totalAmount: "",
+                paymentTerms: "term1",
+                buyerHOD: "hod1",
+                buyerRequester: "req1",
+                remarks: ""
+            });
+            this.getView().setModel(oModel, "PortalModel");
+        },
+
+        onNavBack: function () {
+            var oHistory = sap.ui.core.routing.History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("RouteHome");
+            }
+        },
+
+        onSaveForm: function () {
+            // Validate form before saving
+            if (this._validateForm()) {
+                MessageToast.show("Form saved successfully");
+                // Add your save logic here
+            }
+        },
+
+        onSubmitForm: function () {
+            // Validate form before submitting
+            if (this._validateForm()) {
+                MessageBox.confirm("Are you sure you want to submit this form?", {
+                    title: "Confirm Submission",
+                    onClose: function (oAction) {
+                        if (oAction === MessageBox.Action.OK) {
+                            MessageToast.show("Form submitted successfully");
+                            this._resetForm();
+                        }
+                    }.bind(this)
+                });
+            }
+        },
+
+        _validateForm: function () {
+            var oModel = this.getView().getModel("PortalModel");
+            var oData = oModel.getData();
+
+            var aRequiredFields = [
+                { field: "requesterTokenName", value: oData.requesterTokenName },
+                { field: "department", value: oData.department },
+                { field: "paymentOption", value: oData.paymentOption },
+                { field: "paymentType", value: oData.paymentType },
+                { field: "poNonPO", value: oData.poNonPO },
+                { field: "paymentDate", value: oData.paymentDate },
+                { field: "vendorName", value: oData.vendorName },
+                { field: "invoiceNumber", value: oData.invoiceNumber },
+                { field: "costCenter", value: oData.costCenter },
+                { field: "wbs", value: oData.wbs },
+                { field: "baseAmount", value: oData.baseAmount },
+                { field: "buyerHOD", value: oData.buyerHOD },
+                { field: "buyerRequester", value: oData.buyerRequester }
+            ];
+
+            var bIsValid = true;
+
+            aRequiredFields.forEach(function (oField) {
+                if (!oField.value || oField.value === "") {
+                    MessageToast.show(oField.field + " is a required field");
+                    bIsValid = false;
+                }
+            });
+
+            return bIsValid;
+        },
+
+        _resetForm: function () {
+            // Reset the form to initial state
+            var oModel = this.getView().getModel("PortalModel");
+            oModel.setData({
+                requesterTokenName: "50002151 - PRIYATHAM TELAPROLU",
+                department: "TEMP",
+                date: "2023-08-02",
+                reqNo: "REQ12345",
+                paymentOption: "",
+                paymentType: "",
+                poNonPO: "",
+                paymentDate: "",
+                vendorName: "",
+                accountingDocNumber: "",
+                po: "",
+                vendorCode: "",
+                invoiceNumber: "",
+                costCenter: "",
+                wbs: "",
+                baseAmount: "",
+                gst: "",
+                tds: "",
+                totalAmount: "",
+                paymentTerms: "",
+                buyerHOD: "",
+                buyerRequester: "",
+                remarks: ""
+            });
+        }
+    });
+});
+
 UPDATED CODE
 
 sap.ui.define([
